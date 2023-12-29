218c8b1baa1bcb9429f7a6577b7a8475a9f3acb9
Mohammad Amin Yari <150509197+amin98yari@users.noreply.github.com>
2023-12-29 22:53:35 +0330

    Create complete or not.cpp

---
+++ complete or not.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+int complete(int n)
+{
+int i,sum=0;
+for(i=1;i<=n/2;i++)
+ if(n%i==0)
+ sum+=i; // or sum=sum+i;
+if(sum==n)
+return(1); // or return 1;
+else
+return(0);
+}
+void main()
+{
+int n;
+cout<<"Enter Number";
+cin>>n;
+if(complete(n)==1) // or if(complete(n))
+cout<<"it`s complete";
+else
+cout<<"it`s not complete";
+}
